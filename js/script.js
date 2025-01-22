f122410fb4cf4308e2858765b073494618ec3bd2
Meena Nielsen <145030544+MeenaNielsen@users.noreply.github.com>
2025-01-22 14:33:48 +0100

    updated hjemme arbejder

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
 "use strict";
+
+
+//birthyear
+
+let birthdate = prompt(`What is your birth year?`);
+const  birthYear =parseInt(`${birthdate}`);
+console.log(`${birthYear}`)
+/* current year */
+const date = new Date();
+let currentYear = date.getFullYear();
+console.log(`${currentYear}`);
+
+//calculate the current age
+
+let calculatedAge=0;
+	calculatedAge =currentYear-birthYear;
+	
+	console.log(`${calculatedAge}`);
+	/* let currentYear= new Date(document.getElementById("currentAge").value); */
+/* 	presentParseAge = parseInt(presentAge);
+	console.log(`${presentParseAge}`) */
+	const calAgeInnerHtml =document.getElementById("currentAge").innerHTML = calculatedAge;
+
+	/* document.querySelector("#calculatedAge").appendChild(h1); */
+
+	
+
+
